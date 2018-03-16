0bc002e9659a3cf50e59b93a6e8e3fdd33329d45
Sadie <sadiefreeman@gmail.com>
2018-03-16 18:33:02 +0000

    Made pretty colored text

---
+++ index.js
@@ -0,0 +1,3 @@
+var chalk = require("chalk");
+var message = (chalk.red("Red ") + chalk.bgMagenta("bgMagenta ") + chalk.yellow("Yellow ") + chalk.green("Green ") + chalk.blue("Blue "));
+console.log(message);
